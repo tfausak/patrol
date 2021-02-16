42c9f92d947c89113531addadac04eecfa6c6d37
Taylor Fausak <taylor@fausak.me>
2021-02-16 07:50:45 -0500

    Add request information

--- src/lib/Patrol/Type/Event.hs
+++ src/lib/Patrol/Type/Event.hs
@@ -14,6 +14,7 @@ import qualified Patrol.Type.EventId as EventId
 import qualified Patrol.Type.Exception as Exception
 import qualified Patrol.Type.Level as Level
 import qualified Patrol.Type.Platform as Platform
+import qualified Patrol.Type.Request as Request
 import qualified Patrol.Type.Timestamp as Timestamp
 import qualified Patrol.Utility.Json as Json
 
@@ -30,6 +31,7 @@ data Event = Event
   , modules :: Maybe (Map.Map Text.Text Text.Text)
   , platform :: Platform.Platform
   , release :: Maybe Text.Text
+  , request :: Maybe Request.Request
   , serverName :: Maybe Text.Text
   , tags :: Maybe (Map.Map Text.Text Text.Text)
   , timestamp :: Timestamp.Timestamp
@@ -49,6 +51,7 @@ instance Aeson.ToJSON Event where
     , Json.pair "modules" <$> modules event
     , Just . Json.pair "platform" $ platform event
     , Json.pair "release" <$> release event
+    , Json.pair "request" <$> request event
     , Json.pair "server_name" <$> serverName event
     , Json.pair "tags" <$> tags event
     , Just . Json.pair "timestamp" $ timestamp event
@@ -71,6 +74,7 @@ new = IO.liftIO $ do
     , modules = Nothing
     , platform = Platform.Haskell
     , release = Nothing
+    , request = Nothing
     , serverName = Nothing
     , tags = Nothing
     , timestamp
---
+++ src/lib/Patrol/Type/Request.hs
@@ -0,0 +1,30 @@
+module Patrol.Type.Request
+  ( Request(..)
+  ) where
+
+import qualified Data.Aeson as Aeson
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
+import qualified Data.Text as Text
+import qualified Patrol.Utility.Json as Json
+
+data Request = Request
+  { cookies :: Maybe (Map.Map Text.Text Text.Text)
+  , data_ :: Maybe Aeson.Value
+  , env :: Maybe (Map.Map Text.Text Text.Text)
+  , headers :: Maybe (Map.Map Text.Text Text.Text)
+  , method :: Maybe Text.Text
+  , queryString :: Maybe (Map.Map Text.Text Text.Text)
+  , url :: Maybe Text.Text
+  } deriving (Eq, Show)
+
+instance Aeson.ToJSON Request where
+  toJSON request = Aeson.object $ Maybe.catMaybes
+    [ Json.pair "cookies" <$> cookies request
+    , Json.pair "env" <$> env request
+    , Json.pair "headers" <$> headers request
+    , Json.pair "data" <$> data_ request
+    , Json.pair "method" <$> method request
+    , Json.pair "query_string" <$> queryString request
+    , Json.pair "url" <$> url request
+    ]
