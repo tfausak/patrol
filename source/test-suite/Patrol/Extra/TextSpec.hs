f89a74cfecf925cb2c4e8fe811153bd5e26487d7
Taylor Fausak <taylor@fausak.me>
2022-09-07 18:01:02 -0500

    Import "extra" modules as `Extra` in tests

--- source/test-suite/Patrol/Extra/TextSpec.hs
+++ source/test-suite/Patrol/Extra/TextSpec.hs
@@ -1,19 +1,19 @@
 module Patrol.Extra.TextSpec where
 
 import qualified Data.Text as Text
-import qualified Patrol.Extra.Text as Text
+import qualified Patrol.Extra.Text as Extra
 import qualified Test.Hspec as Hspec
 
 spec :: Hspec.Spec
 spec = Hspec.describe "Patrol.Extra.Text" $ do
   Hspec.describe "presence" $ do
     Hspec.it "returns nothing when the text is empty" $ do
-      Text.presence Text.empty `Hspec.shouldBe` Nothing
+      Extra.presence Text.empty `Hspec.shouldBe` Nothing
 
     Hspec.it "returns just when the text is all spaces" $ do
       let text = Text.singleton ' '
-      Text.presence text `Hspec.shouldBe` Nothing
+      Extra.presence text `Hspec.shouldBe` Nothing
 
     Hspec.it "returns just when the text is not empty" $ do
       let text = Text.singleton 'x'
-      Text.presence text `Hspec.shouldBe` Just text
+      Extra.presence text `Hspec.shouldBe` Just text
