57cb0e053eb8f0863ad22d657ad55fa295915ca3
Taylor Fausak <taylor@fausak.me>
2023-01-26 13:16:22 +0000

    Upgrade to GHC 9.4.4

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,8 +1,4 @@
 #! /usr/bin/env sh
 set -o errexit -o xtrace
 
-volume=cabal-store
-if ! docker volume inspect "$volume"
-then
-  docker volume create "$volume"
-fi
+docker volume create cabal-store
--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,11 +1,9 @@
 #! /usr/bin/env sh
 set -o errexit -o xtrace
 
-cabal --ignore-project update
+cabal update
 
 if ! test -f cabal.project.local
 then
   cabal configure --disable-optimization --enable-tests --jobs --test-show-details direct
 fi
-
-cabal update
