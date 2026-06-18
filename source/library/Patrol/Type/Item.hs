424e5fba868248d08a285661104b2f1f41539b92
jkachmar <j@mercury.com>
2026-06-18 11:52:23 -0400

    Avoid unnecessary copy during Item serialization

--- source/library/Patrol/Type/Item.hs
+++ source/library/Patrol/Type/Item.hs
@@ -4,9 +4,9 @@ module Patrol.Type.Item where
 
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.Key as Key
-import qualified Data.ByteString as ByteString
 import qualified Data.ByteString.Builder as Builder
 import qualified Data.ByteString.Lazy as LazyByteString
+import Data.Int (Int64)
 import qualified Patrol.Type.ClientReport as ClientReport
 import qualified Patrol.Type.Event as Event
 
@@ -27,17 +27,20 @@ data Item
 
 serialize :: Item -> Builder.Builder
 serialize item = case item of
-  Event event ->
-    let payload = LazyByteString.toStrict $ Aeson.encode event
-        headers = buildHeaders "event" (ByteString.length payload)
-     in headers <> Builder.char7 '\n' <> Builder.byteString payload
-  ClientReport clientReport ->
-    let payload = LazyByteString.toStrict $ Aeson.encode clientReport
-        headers = buildHeaders "client_report" (ByteString.length payload)
-     in headers <> Builder.char7 '\n' <> Builder.byteString payload
+  Event event -> envelopeItem "event" (Aeson.encode event)
+  ClientReport clientReport -> envelopeItem "client_report" (Aeson.encode clientReport)
   Raw -> mempty
   where
-    buildHeaders :: Key.Key -> Int -> Builder.Builder
+    -- Keep the encoded payload lazy: take the item-header length from the lazy
+    -- chunks and stream them straight into the builder, rather than forcing a
+    -- contiguous strict copy of every payload.
+    envelopeItem :: Key.Key -> LazyByteString.ByteString -> Builder.Builder
+    envelopeItem type_ payload =
+      buildHeaders type_ (LazyByteString.length payload)
+        <> Builder.char7 '\n'
+        <> Builder.lazyByteString payload
+
+    buildHeaders :: Key.Key -> Int64 -> Builder.Builder
     buildHeaders type_ length_ =
       Aeson.fromEncoding $
         Aeson.pairs ("type" Aeson..= type_ <> "length" Aeson..= length_)
